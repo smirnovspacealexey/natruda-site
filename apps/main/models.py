f9ca5fa4b2a1791232106fd02f27a8307bd1dfad
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2022-09-27 12:15:28 +0500

    fix counter

--- apps/main/models.py
+++ apps/main/models.py
@@ -6,6 +6,7 @@ class Point(models.Model):
     name = models.CharField(max_length=200, verbose_name="Имя точки")
     address = models.CharField(max_length=500, verbose_name="Адрес точки")
     volume = models.IntegerField(default=100, verbose_name="рубли")
+    picture = models.ImageField(upload_to="img/points", blank=True, null=True, verbose_name="картинка")
     ordering = models.IntegerField('ordering', default=0)
 
     def __str__(self):
