44eda685dc0b08998d96ff272aca19625bce6070
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2022-10-04 17:46:48 +0500

    add queue command

--- customer_interface/management/commands/queue.py
+++ customer_interface/management/commands/queue.py
@@ -1,11 +1,19 @@
 from django.core.management.base import BaseCommand
 from customer_interface.models import *
+import requests
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print('--------To Quene---------')
+        print('--------ToQueue---------')
 
+        # url = 'http://127.0.0.1:8001/shaw_queue/order-from-site/'
+        url = 'http://shawarma.natruda/shaw_queue/order-from-site/'
+        data = {'content': [{'id': 17, 'title': 'Соус Белый', 'price': 50, 'quantity': 1, 'note': 'НЕ ГОТОВИТЬ. ТЕСТОВЫЙ ЗАКАЗ!!!!!'}],
+                'delivery': True}
+
+        res = requests.post(url, json=data)
+        print(res)
         print('---------------------')
 
 
