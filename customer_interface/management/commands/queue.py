502566ad33181408a6de260370e354a5a166a856
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2022-10-04 17:22:14 +0500

    add queue command

---
+++ customer_interface/management/commands/queue.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from customer_interface.models import *
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        print('--------To Quene---------')
+
+        print('---------------------')
+
+
+
