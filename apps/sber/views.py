468e403939b39191bbfd137f102a6c5b77aa6b9f
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2023-04-22 20:50:24 +0500

    add sber_result

--- apps/sber/views.py
+++ apps/sber/views.py
@@ -66,9 +66,9 @@ def sber_result(request):
     try:
         logger_debug.info(f'sber_result\n----\n {request.GET}\n{request.GET.get("operation")}')
         logger_debug.info(f"{request.GET.get('operation') == 'deposited' and request.GET.get('status') == '1'}")
-
-        result = requests.get('http://shawarma.natruda/shaw_queue/send_customers_menu/')
-        logger_debug.info(f'result\n {result}\n')
+        #
+        # result = requests.get('http://shawarma.natruda/shaw_queue/send_customers_menu/')
+        # logger_debug.info(f'result\n {result}\n')
 
         if request.GET.get('operation') == 'deposited' and request.GET.get('status') == '1':
             data = {
