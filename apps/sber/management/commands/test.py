ea3fd5afdc0ca53122c5e50ada8bd76f5fde39be
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2023-04-23 10:55:50 +0500

    add sber_result

---
+++ apps/sber/management/commands/test.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
+from django.contrib.sessions.models import Session
+import logging
+import sys, traceback
+import requests
+
+logger_debug = logging.getLogger('debug_logger')
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        try:
+            res = requests.get('http://shawarma.natruda/sber/result?daily_number=881533')
+            logger_debug.info(f'res\n {res}\n')
+            print(res)
+
+        except:
+            logger_debug.info(f'delivery_request ERROR: {traceback.format_exc()}')
+            print(traceback.format_exc())
+
+
